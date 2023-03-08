bd736ae5fb39c653de6d15e84f9eedf647db73c4
Mauricio Gustavo Rojas Solis <gus.solis.15@gmail.com>
2023-03-08 19:35:05 -0400

    Power up: shield

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,7 +1,16 @@
 import pygame
 
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, DUCKING, JUMPING
+from dino_runner.utils.constants import (
+    RUNNING, 
+    DUCKING, 
+    JUMPING,
+    DEFAULT_TYPE,
+    SHIELD_TYPE,
+    DUCKING_SHIELD,
+    RUNNING_SHIELD,
+    JUMPING_SHIELD
+    )
 
 class Dinosaur(Sprite):
     POS_X = 80
@@ -10,7 +19,12 @@ class Dinosaur(Sprite):
     JUMP_VEL = 8.5
 
     def __init__(self):
-        self.image = RUNNING[0]
+        self.run_img = {DEFAULT_TYPE: RUNNING, SHIELD_TYPE: RUNNING_SHIELD}
+        self.duck_img = {DEFAULT_TYPE: DUCKING, SHIELD_TYPE: DUCKING_SHIELD}
+        self.jump_img = {DEFAULT_TYPE: JUMPING, SHIELD_TYPE: JUMPING_SHIELD}
+        self.type = DEFAULT_TYPE
+        self.image = self.run_img[self.type][0]
+
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.POS_X
         self.dino_rect.y = self.POS_Y
@@ -19,6 +33,12 @@ class Dinosaur(Sprite):
         self.dino_duck = False
         self.dino_jump = False
         self.jump_vel = self.JUMP_VEL
+        self.setup_states()
+
+    def setup_states(self):
+        self.has_power_up = False
+        self.shield = False
+        self.shield_time_up = 0
 
     def update(self, user_input):
         if self.dino_jump:
@@ -48,20 +68,21 @@ class Dinosaur(Sprite):
         screen.blit(self.image, self.dino_rect)
 
     def run(self):
-        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.image = self.run_img[self.type][self.step_index // 5]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.POS_X
         self.dino_rect.y = self.POS_Y
         self.step_index += 1
+    
     def duck(self):
-        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.image = self.duck_img[self.type][self.step_index // 5]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.POS_X
         self.dino_rect.y = self.POS_Y_DUCKING
         self.step_index += 1
 
     def jump(self):
-        self.image = JUMPING
+        self.image = self.jump_img[self.type]
         if self.dino_jump:
             self.dino_rect.y -= self.jump_vel * 4 # Salto
             self.jump_vel -= 0.8 # Salto, cuando llega a negativo, baja
@@ -69,3 +90,14 @@ class Dinosaur(Sprite):
             self.dino_rect.y = self.POS_Y
             self.dino_jump = False
             self.jump_vel = self.JUMP_VEL
+
+    def check_invincibility(self):
+         if self.shield:
+             time_to_show = round((self.shield_time_up - pygame.time.get_ticks()) / 1000, 2)#redondea para saber si hay tiempo para mostrar
+             if not time_to_show >= 0:
+                self.shield = False
+                self.update_to_default(SHIELD_TYPE)
+
+    def update_to_default (self, current_type):
+         if self.type == current_type:
+             self.type = DEFAULT_TYPE
